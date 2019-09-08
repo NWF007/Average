969c59141f7982c8f06ee481dfb69b0ff9f32d92
NWF007 <53278049+NWF007@users.noreply.github.com>
2019-09-08 20:06:49 +0200

    Add files via upload

---
+++ grader.js
@@ -0,0 +1,18 @@
+function average(scores){
+	var sum = 0;
+	
+	for(var i = 0; i < scores.length; i++){
+		sum += scores[i];
+	}
+	
+	var avg = sum / scores.length;
+	
+	//console.log(Math.round(avg));
+	return Math.round(avg);
+}
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores));
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores2));
